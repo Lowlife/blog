9b6f4b0f9f30b13a35514fd0c1b2dcf5933ea678
Lowlife <lowlife171@gmail.com>
2012-07-12 12:55:56 +0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'pg'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'f6cf087dbdcaff907bb14c0f1bbc73a91e1f4f6128a109e39b0417db8eb3b054d4f8f020427656e2454bc9ad17a070058107735e75fc6f86cfea2bdf10c743dc'
